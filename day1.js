d04792c93147abb5083abc689700c0a257ce17a8
tucie <mail@cliffconklin.com>
2019-12-10 17:21:01 -0500

    Day 1 solutions
    Solutions for Day 1 parts 1 and 2.

---
+++ day1.js
@@ -0,0 +1,48 @@
+// Day 1
+var fs = require('fs')
+
+try {
+    var contents = fs.readFileSync('./day1_input.txt', 'utf8')
+} catch (err) {
+    console.error(err)
+}
+const input = contents.split('\n')
+console.log(input)
+
+class fuelCalculator {
+    constructor() {
+        this.totalFuel = 0
+        this.moduleFuel = 0
+    }
+    fuelRequiredForModule(mass) {
+        this.totalFuel = this.totalFuel + (Math.floor(mass / 3) - 2)
+    }
+    fuelRequiredForModuleRecur(mass) {
+        if (mass < 7) {
+            return this.moduleFuel
+        }
+        let fuel = Math.floor(mass / 3) - 2
+        this.moduleFuel = this.moduleFuel + fuel
+        //console.log (fuel)
+        return this.fuelRequiredForModuleRecur(fuel)
+    }
+    getTotalFuel() {
+        return this.totalFuel
+    }
+}
+
+let fuelNeeded = new fuelCalculator()
+for (let i = 0; i < input.length; i++) {
+    fuelNeeded.fuelRequiredForModule(input[i])
+}
+console.log(`Day 1 Part One Answer: ${fuelNeeded.totalFuel}`)
+
+let fuelNeededRecur = new fuelCalculator()
+for (let i = 0; i < input.length; i++) {
+    let moduleFuel = fuelNeededRecur.fuelRequiredForModuleRecur(input[i])
+    fuelNeededRecur.totalFuel += moduleFuel
+
+    // Reset module fuel for next time
+    fuelNeededRecur.moduleFuel = 0
+}
+console.log(`Day 1 Part Two Answer: ${fuelNeededRecur.totalFuel}`)
